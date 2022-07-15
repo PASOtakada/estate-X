ce2843447894357b2b89d595c0ba505a1adb2a72
MTR <takada@paso-group.com>
2022-07-15 15:50:00 +0900

    初期化しました。

---
+++ app/src/main/java/com/paso_group/estate_x/MainActivity.java
@@ -0,0 +1,14 @@
+package com.paso_group.estate_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
